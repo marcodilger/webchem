14d7410ba5d38356e94850735f6426026653cf70
Marco Dilger <marcodilger@gmx.de>
2019-10-23 07:55:38 +0200

    extraction works for single substance. figuring out the best return format

--- R/pubchem-test.R
+++ R/pubchem-test.R
@@ -1,23 +1,24 @@
 library(httr)
 library(jsonlite)
-library(tidyverse) #to avoid
 library(xml2)
+library(stringr)
+
 
 # pug-rest failed (returns huge objects)
 # pug view via JSON would work, but quite complicated to reach the right nodes
 # try with xml response
 
-classification_query <- "https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/2244/XML?heading=GHS+Classification"
+classification_query <- "https://pubchem.ncbi.nlm.nih.gov/rest/pug_view/data/compound/6341/XML?heading=GHS+Classification"
 
 cont <- try(content(POST(classification_query),
                     type = 'text', encoding = 'UTF-8'),
             silent = TRUE
 )
 
-# looks much!! easier accessible via xpaths
 
 ghs_xml <- read_xml(cont)
 
+ghs_xml <- xml_ns_strip(ghs_xml)
 xml_children(ghs_xml)
 
 # of interest are all 
@@ -25,27 +26,57 @@ xml_children(ghs_xml)
 # within <Information> Tags that have a 
 # <Name>GHS Hazard Statements</Name>
 
-ghs_xml <- xml_ns_strip(ghs_xml)
 
-xml_find_all(ghs_xml, "Section")
-ghs_info <- xml_find_all(ghs_xml, xpath = "//Information[Name][contains(., 'GHS Hazard Statements')]//StringWithMarkup/String")
-xml_find_all(ghs_xml, xpath = "//Information[ReferenceNumber][contains(., '52')][Name][contains(., 'GHS Hazard Statements')]")
-
-xml_text(ghs_info)
-
-# extract all available references
+# extract all available references containing GHS hazard statements
 ghs_refs <- xml_text(xml_find_all(ghs_xml, xpath = "//Information[Name][contains(., 'GHS Hazard Statements')]//ReferenceNumber"))
-
+# note: returned reference ID are not consistent accross substance, reference ID is not suitable as identifier of sources
 
 # access hazard statements of referencenumbers directly
-xml_find_all(ghs_xml, xpath = "//Information[ReferenceNumber][contains(., '52')][Name][contains(., 'GHS Hazard Statements')]//StringWithMarkup/String")
+#response <- xml_find_all(ghs_xml, xpath = "//Information[ReferenceNumber[text()='52']][Name][contains(., 'GHS Hazard Statements')]//StringWithMarkup/String") # <- works
+#xml_text(response)
 
-# dynamically
+# dynamically get GHS statements
 for (ref in ghs_refs) {
-  print(
-    xml_find_all(ghs_xml, xpath = paste0("//Information[ReferenceNumber][contains(., ", ghs_refs[ref], ")][Name][contains(., 'GHS Hazard Statements')]//StringWithMarkup/String") )
-  )
-} # error, spits out everything for all references
+  print(ref)
+  response <- xml_find_all(ghs_xml, xpath = paste0("//Information[ReferenceNumber[text()='", ref, "']][Name][contains(., 'GHS Hazard Statements')]//StringWithMarkup/String") )
+  print(response)
+}
+
+
+# build response list for each reference
+
+response <-  list()
+for(ref in ghs_refs) {
+
+source <- xml_find_all(ghs_xml, xpath = paste0("Reference[ReferenceNumber[text()='", ref, "']]"))
+source_name <- xml_text(xml_find_all(source, xpath = "SourceName"))
+source_substancename <- xml_text(xml_find_all(source, xpath = "Name"))
+source_description <- xml_text(xml_find_all(source, xpath = "Description"))
+source_url <- xml_text(xml_find_all(source, xpath = "URL"))
+source_license <- xml_text(xml_find_all(source, xpath = "LicenseNote"))
+
+hazard_statements <- xml_text(xml_find_all(ghs_xml, 
+                      xpath = paste0("//Information[ReferenceNumber[text()='", ref, "']][Name][contains(., 'GHS Hazard Statements')]//StringWithMarkup/String")
+                      )
+         )
+hazard_statements <- hazard_statements[hazard_statements != ""]
+hazard_codes <- str_extract(hazard_statements, "^H\\d{3}") # needs to be improved to capture minimum classification and subclasses as well, see https://en.wikipedia.org/wiki/GHS_hazard_statements
+hazard_codes <- sort(unique(hazard_codes[hazard_codes != ""]))
+
+
+
+info <- list(source_name = source_name, substance = source_substancename, codes = hazard_codes, hazard_statements = hazard_statements, source_desc = source_description, url = source_url, license = source_license)
+
+response[[length(response) + 1]] <-  info # ansprechen via info$name ist KEINE gute Idee (z.B. ECHA doppelte beiträge möglich)
+}
+
+
+response[2]
+
+# ToDo: variable/list with sources to extract: named specificaly (returning empty when not available)
+# or option 'all' for all available
+  # needed for this: explicit list of names which can occur as sources
+
 
 
 # entweder es wird an der Stelle dann allgemein gehalten,
